2b159a45e0cc2d39340f0983897ee5026932044b
Petre Fredian Gradinaru <petrefredian.gradinaru@lexisnexisrisk.com>
2023-01-25 13:42:13 +0200

    change output

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
-output "elk1_public_dns" {
+output "elk1_private_dns" {
   description = "The public DNS name assigned to the instance. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
-  value       = aws_instance.elk1.public_dns
+  value       = aws_instance.elk1.private_dns
 }
 
-output "elk2_public_dns" {
+output "elk2_private_dns" {
   description = "The public DNS name assigned to the instance. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
-  value       = aws_instance.elk2.public_dns
+  value       = aws_instance.elk2.private_dns
 }
 
-output "elk3_public_dns" {
+output "elk3_private_dns" {
   description = "The public DNS name assigned to the instance. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
-  value       = aws_instance.elk3.public_dns
+  value       = aws_instance.elk3.private_dns
 }
 
-output "ansible_public_dns" {
+output "ansible_private_dns" {
   description = "The public DNS name assigned to the instance. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
-  value       = aws_instance.ansible.public_dns
+  value       = aws_instance.ansible.private_dns
 }
 
-output "apache-ec2_public_dns" {
+output "apache-ec2_private_dns" {
   description = "The public DNS name assigned to the instance. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
-  value       = aws_instance.apache-ec2.public_dns
+  value       = aws_instance.apache-ec2.private_dns
 }
 
-output "syslog-ec2_public_dns" {
+output "syslog-ec2_private_dns" {
   description = "The public DNS name assigned to the instance. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
-  value       = aws_instance.syslog-ec2.public_dns
+  value       = aws_instance.syslog-ec2.private_dns
 }
